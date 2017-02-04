bb9282fd1519ca98edbe4dec04afc2e07f368d51
acecode009 <acecode009@gmail.com>
2017-02-04 23:55:16 +0200

    Made api public

--- server/index.js
+++ server/index.js
@@ -13,6 +13,14 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 
+
+//CORS Support
+app.use(function(rq,res,next){
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-headers', 'Content-Type');
+    next();
+})
 //connect to mongoDB
 mongoose.connect('mongodb://localhost/meanapp');
 mongoose.connection.once('open', function(){
